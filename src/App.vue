5a1f8fb4c6763320349f84af51f998dce79ee3f5
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2023-11-18 11:57:25 -0300

    iniciando tela de login

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <RouterView/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+
+:root {
+  --cor-de-fundo: #F4F4F4;
+  --cor-primaria: #ce2828;
+  --cor-secundaria: #af2525;
+}
+
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+body {
+  background-color: var(--cor-de-fundo);
+}
+
+button {
+  background-color: var(--cor-primaria);
+  cursor: pointer;
+  border-radius: 10px;
+  min-height: 40px;
+  color: white;
+  font-weight: bold;
+  border: none;
+  padding: 10px;
+  box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+}
+
+button:hover {
+  background-color: var(--cor-secundaria);
+}
+
+
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="tela-de-login">
+        <div class="login-container">
+            <div>
+                <img class="logo" src="../assets/logo.png" alt="logo">
+            </div>
+            Entrar como:
+
+            <div class="botoes">
+                <button>
+                    Usuário comum
+                </button>
+                <button>
+                    Administrador
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.tela-de-login {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.login-container {
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    height: 450px;
+    width: 400px;
+    border-radius: 8px;
+    padding: 10px;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+}
+
+.logo {
+    height: 250px;
+    width: 250px;
+}
+
+.botoes {
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
