1765fe15386f2734df430d5ee2d915f778b36345
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2023-11-18 18:12:21 -0300

    melhorando modal

--- src/components/ModalMontarSeuLanche.vue
+++ src/components/ModalMontarSeuLanche.vue
@@ -2,15 +2,19 @@
     <div :class="{ 'is-active': isActive }" class="modal">
         <div class="modal-background"></div>
         <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">Modal title</p>
+            <header class="modal-card-head vermelho" style="background-color: var(--cor-primaria);">
+                <h1 class="modal-card-title" style="color: white;">Monte seu próprio lanche</h1>
                 <button class="delete" aria-label="close" @click="fecharModal()"></button>
             </header>
             <section class="modal-card-body">
+                <p>
+                    Aqui no HotDog Gourmet, você tem o poder de personalizar o seu próprio lanche! 
+                    Escolha os ingredientes à sua preferência e descubra o preço exato do seu delicioso 
+                    hot dog personalizado!
+                </p>
             </section>
-            <footer class="modal-card-foot">
-                <button class="button is-success">Save changes</button>
-                <button class="button" @click="fecharModal()">Cancel</button>
+            <footer class="modal-card-foot" style="display: flex; flex-direction: row-reverse;">
+                <button style="width: 100px;" @click="fecharModal()">Concluir</button>
             </footer>
         </div>
     </div>
@@ -32,3 +36,12 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+p {
+    font-style: italic;
+    cursor: default;
+}
+
+</style>
